deefb0451629f708eab82f80c8b63b1118884240
thabtom <tsehayegh@gmail.com>
2018-03-02 16:39:28 -0500

    updated recipe PUT testing

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -67,9 +67,6 @@ describe('Recipes', function(){
       })
       .then(function(res) {
         expect(res).to.have.status(204);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.deep.equal(updateData);
       });
   });
 
